7c81052cc5fafc122786a41a87ab32e68e810a05
sandromatiass <devsandromatias83@gmail.com>
2023-10-04 16:32:41 -0300

    docs: args

---
+++ my-first-project-java/src/activities/classromSeven/AboutMe.java
@@ -0,0 +1,13 @@
+package activities.classromSeven;
+
+public class AboutMe {
+  
+  public static void main(String[] args){
+
+    String firstName = args[0];
+    String secondName = args[1];
+    int age = Interger.valueOf(args[2]);
+    double altura = Double.valueOf(args[3]);
+    
+  }
+}
---
+++ my-first-project-java/src/activities/classromSeven/MyClass.java
@@ -0,0 +1,8 @@
+package activities.classromSeven;
+
+public class MyClass {
+  public static void main(String[] args){
+    System.out.print("Hi, I ran it throusgh the terminal.");
+  }
+  
+}
