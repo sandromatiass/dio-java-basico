db5261db19bc66b3b8a569cf9c753806ee4ffbfb
Sandro Matias <devsandromatias@gmail.com>
2023-10-04 22:11:22 -0300

    feat: course completed.

--- my-first-project-java/src/activities/classromSeven/AboutMe.java
+++ my-first-project-java/src/activities/classromSeven/AboutMe.java
@@ -1,13 +1,30 @@
 package activities.classromSeven;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+
 public class AboutMe {
   
   public static void main(String[] args){
 
-    String firstName = args[0];
-    String secondName = args[1];
-    int age = Interger.valueOf(args[2]);
-    double altura = Double.valueOf(args[3]);
-    
+    Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+    System.out.println("Type your height: ");
+    double height = scanner.nextDouble();
+
+    System.out.print("Type your first name: ");
+    String firstName = scanner.next();
+
+    System.out.print("Type your second name: ");
+    String secondName = scanner.next();
+
+    System.out.print("Type your age: ");
+    int age = scanner.nextInt();
+ 
+
+    System.out.println("Hi, my name is " + firstName + " " + secondName);
+    System.out.println("I have " + age + " years ");
+    System.out.println("My height is " + height + " cm ");
   }
 }
